2058edb7aaf23051979d9860e59355482c8d5f8e
Mike James <mikejames@Mikes-MacBook-Pro.local>
2017-03-31 13:12:17 +1100

    initial commit for plugin

---
+++ index.js
@@ -0,0 +1,34 @@
+const keystone = require('keystone');
+const Router = require('express').Router;
+const forgotPassword = require('./forgotPassword');
+const changePassword = require('./changePassword');
+const Types = keystone.Field.Types;
+
+const internals = {};
+
+module.exports = internals.plugin = function keystoneForgottenPassword (config) {
+	const routes = new Router();
+	const RESET_PASSWORD_KEY_EXPIRY = config.keyExpiry || 24;
+	const { onForgotEmail = Promise.resolve(), onChangePasswordEmail = Promise.resolve() } = config;
+
+
+	routes.post('/forgot', forgotPassword({
+		onForgotEmail,
+		RESET_PASSWORD_KEY_EXPIRY,
+	}));
+
+	routes.post('/change-password', changePassword({
+		onChangePasswordEmail,
+		RESET_PASSWORD_KEY_EXPIRY,
+	}));
+
+	return routes;
+};
+
+internals.plugin.enhanceUserModel = function enhanceUserModel (Model) {
+	require('./model/ForgotPassword');
+	Model.add({
+		passwordLastUpdated: { type: Date },
+		forgotPasswordRequests: { type: Types.Relationship, ref: 'ForgotPassword', many: true, hidden: true },
+	});
+};
